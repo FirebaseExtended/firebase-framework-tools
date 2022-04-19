8dbe0b58890b9a4ae3f63b7191841d2ad49cc23a
James Daniels <jamesdaniels@google.com>
2022-04-19 14:49:15 -0400

    Starting Github Actions work

---
+++ build.sh
@@ -0,0 +1,21 @@
+SHORT_SHA=$(git rev-parse --short $GITHUB_SHA)
+TAG_TEST="^refs/tags/.+$"
+LATEST_TEST="^[^-]*$"
+
+if [[ $GITHUB_REF =~ $TAG_TEST ]]; then
+    OVERRIDE_VERSION=${GITHUB_REF/refs\/tags\//}
+    if [[ $OVERRIDE_VERSION =~ $LATEST_TEST ]]; then
+        NPM_TAG=latest
+    else
+        NPM_TAG=next
+    fi;
+else
+    OVERRIDE_VERSION=$(node -e "console.log(require('./package.json').version)")-canary.$SHORT_SHA
+    NPM_TAG=canary
+fi;
+
+npm --no-git-tag-version --allow-same-version -f version $OVERRIDE_VERSION
+
+npm run build &&
+    echo "npm publish . --tag $NPM_TAG" > ./dist/publish.sh &&
+    chmod +x ./dist/publish.sh
