4cabd8f8196d865ad8171b472aca80a023e74afe
James Daniels <jamesdaniels@google.com>
2022-04-23 16:02:34 -0400

    Try a different approach

--- tools/dev-tools.sh
+++ tools/dev-tools.sh
@@ -1,9 +1,7 @@
 # TODO don't run all this if firebase-tools already exists
 npm run clean:dev &&
     export FIREBASE_FRAMEWORKS_TARBALL=$(pwd)/$(npm pack .) &&
-    tar -xf $(npm pack firebase-tools) &&
-    mv package firebase-tools &&
-    cd firebase-tools &&
-    npm i --save --force --ignore-scripts $FIREBASE_FRAMEWORKS_TARBALL &&
-    npm install --ignore-scripts --force -g . &&
-    cd ..
+    npm i -g firebase-tools &&
+    cd $(npm root -g)/firebase-tools &&
+    npm i --save --force $FIREBASE_FRAMEWORKS_TARBALL &&
+    cd $(pwd)
