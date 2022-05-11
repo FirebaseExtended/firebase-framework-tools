fbc8d5217971c78fdc1ebd08f9a4bf60bedd05ed
James Daniels <jamesdaniels@google.com>
2022-05-11 03:22:11 -0400

    Tests

---
+++ tools/test-angular.ts
@@ -0,0 +1,21 @@
+import { exit } from 'process';
+import { readdir, stat } from 'fs/promises';
+import { join } from 'path';
+
+const site = 'nextjs-demo-73e34';
+
+const run = async () => {
+    if (!(await stat(join('e2e', 'angular', '.firebase'))).isDirectory()) throw '.firebase does not exist';
+    if (!(await stat(join('e2e', 'angular', '.firebase', site))).isDirectory()) throw `.firebase/${site} does not exist`;
+    if (!(await stat(join('e2e', 'angular', '.firebase', site, 'hosting'))).isDirectory()) throw `.firebase/${site}/hosting does not exist`;
+    if (!(await readdir(join('e2e', 'angular', '.firebase', site, 'hosting'))).length) throw `no files in .firebase/${site}/hosting`;
+    if ((await stat(join('e2e', 'angular', '.firebase', site, 'functions'))).isDirectory()) throw `.firebase/${site}/functions should not exist`;
+}
+
+run().then(
+    () => exit(0),
+    err => {
+        console.error(err.message);
+        exit(1);
+    }
+);
---
+++ tools/test-custom.ts
@@ -0,0 +1,19 @@
+import { exit } from 'process';
+import { readdir, stat } from 'fs/promises';
+import { join } from 'path';
+
+const site = 'nextjs-demo-73e34';
+
+const run = async () => {
+    if (!(await stat(join('e2e', 'custom', '.firebase'))).isDirectory()) throw '.firebase does not exist';
+    if (!(await stat(join('e2e', 'custom', '.firebase', site))).isDirectory()) throw `.firebase/${site} does not exist`;
+    if (!(await stat(join('e2e', 'custom', '.firebase', site, 'hosting'))).isDirectory()) throw `.firebase/${site}/hosting does not exist`;
+    if (!(await readdir(join('e2e', 'custom', '.firebase', site, 'hosting'))).length) throw `no files in .firebase/${site}/hosting`;
+    if (!(await stat(join('e2e', 'custom', '.firebase', site, 'functions'))).isDirectory()) throw `.firebase/${site}/functions does not exist`;
+    if (!(await readdir(join('e2e', 'custom', '.firebase', site, 'functions'))).length) throw `no files in .firebase/${site}/functions`;
+}
+
+run().then(
+    () => exit(0),
+    () => exit(1)
+);
