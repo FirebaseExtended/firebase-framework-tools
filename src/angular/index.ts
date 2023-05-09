ccc8af4e153bde2b70a825364c81d6a5e0f0fdfb
James Daniels <jamesdaniels@google.com>
2023-05-09 04:05:55 -0400

    Angular Image optimzation

--- src/angular/index.ts
+++ src/angular/index.ts
@@ -1,1 +1,31 @@
-export * from '../express/index.js';
+import type { Request } from 'firebase-functions/v2/https';
+import type { Response } from 'express';
+
+import { handle as expressHandle } from '../express/index.js';
+import { basename, join, normalize, relative } from 'path';
+import { createReadStream } from 'fs';
+import { mediaTypes } from '@hapi/accept';
+
+export const handle = async (req: Request, res: Response) => {
+    if (basename(req.path) === '__image__') {
+        const src = req.query.src;
+        if (typeof src !== "string") return res.sendStatus(404);
+        let locale = req.query.locale || "";
+        if (typeof locale !== "string") return res.sendStatus(404);
+        const serveFrom = `./dist/hosting/browser/${locale}`;
+        const normalizedPath = normalize(join(serveFrom, src));
+        if (relative(serveFrom, normalizedPath).startsWith("..")) return res.sendStatus(404);
+        const { default: sharp} = await import("sharp");
+        const width = typeof req.query.width === "string" ? parseInt(req.query.width) : undefined;
+        const accepts = mediaTypes(req.headers.accept);
+        const format = accepts.includes('image/webp') ? 'webp' : undefined;
+        let pipeline = sharp().resize({ width });
+        if (format) pipeline[format]();
+        // TODO allow override
+        res.setHeader('Cache-Control', 'public, max-age=3600, s-maxage=86400');
+        res.setHeader('Vary', 'Accept, Accept-Encoding');
+        createReadStream(normalizedPath).pipe(pipeline).pipe(res);
+    } else {
+        await expressHandle(req, res);
+    }
+};
