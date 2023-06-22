f696918735b9aa905e2e55282172d0f96c1748d3
Leonardo Ortiz <leo@monogram.io>
2023-06-22 12:33:42 -0300

    use hostname 0.0.0.0 for Next.js server

--- src/next.js/index.ts
+++ src/next.js/index.ts
@@ -1,38 +1,38 @@
-import { parse } from 'url';
-import { default as next } from 'next';
-import type { Request } from 'firebase-functions/v2/https';
-import type { Response } from 'express';
-import LRU from 'lru-cache';
-import { NextServer } from 'next/dist/server/next.js';
+import { parse } from "url";
+import { default as next } from "next";
+import type { Request } from "firebase-functions/v2/https";
+import type { Response } from "express";
+import LRU from "lru-cache";
+import { NextServer } from "next/dist/server/next.js";
 
 const nextAppsLRU = new LRU<string, NextServer>({
-    // TODO tune this
-    max: 3,
-    allowStale: true,
-    updateAgeOnGet: true,
-    dispose: (server) => {
-        server.close();
-    }
+  // TODO tune this
+  max: 3,
+  allowStale: true,
+  updateAgeOnGet: true,
+  dispose: (server) => {
+    server.close();
+  },
 });
 
 export const handle = async (req: Request, res: Response) => {
-    const { hostname, protocol, url } = req;
-    const port = protocol === 'https' ? 443 : 80;
-    const key = [hostname, port].join(':');
-    // I wish there was a better way to do this, but it seems like this is the
-    // way to go. Should investigate more if we can get hostname/port to be
-    // dynamic for middleware. 
-    let nextApp = nextAppsLRU.get(key);
-    if (!nextApp) {
-        nextApp = next({
-            dev: false,
-            dir: process.cwd(),
-            hostname,
-            port
-        });
-        nextAppsLRU.set(key, nextApp);
-    }
-    await nextApp.prepare();
-    const parsedUrl = parse(url, true);
-    nextApp.getRequestHandler()(req, res, parsedUrl);
+  const { hostname, protocol, url } = req;
+  const port = protocol === "https" ? 443 : 80;
+  const key = [hostname, port].join(":");
+  // I wish there was a better way to do this, but it seems like this is the
+  // way to go. Should investigate more if we can get hostname/port to be
+  // dynamic for middleware.
+  let nextApp = nextAppsLRU.get(key);
+  if (!nextApp) {
+    nextApp = next({
+      dev: false,
+      dir: process.cwd(),
+      hostname: "0.0.0.0",
+      port,
+    });
+    nextAppsLRU.set(key, nextApp);
+  }
+  await nextApp.prepare();
+  const parsedUrl = parse(url, true);
+  nextApp.getRequestHandler()(req, res, parsedUrl);
 };
