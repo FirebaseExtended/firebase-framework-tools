84efb70390414a63457f158eafdf576f85617c48
Julien Martel <julien@monogram.io>
2023-08-09 19:41:18 -0500

    test nuxt3-test fix

--- tools/test-custom.ts
+++ tools/test-custom.ts
@@ -8,24 +8,43 @@ const site = 'nextjs-demo-73e34';
 const cwd = join('e2e', 'custom');
 
 const run = async () => {
-    execSync('firebase emulators:exec "exit 0"', { cwd });
-    if (await access(join(cwd, '.firebase')).then(() => false, () => true)) throw '.firebase does not exist';
-    if (await access(join(cwd, '.firebase', site)).then(() => false, () => true)) throw `.firebase/${site} does not exist`;
-    if (await access(join(cwd, '.firebase', site, 'hosting')).then(() => false, () => true)) throw `.firebase/${site}/hosting does not exist`;
-    if (!(await readdir(join(cwd, '.firebase', site, 'hosting'))).length) throw `no files in .firebase/${site}/hosting`;
-    // TODO figure out what is wrong
-    // if (await access(join(cwd, '.firebase', site, 'functions')).then(() => false, () => true)) throw `.firebase/${site}/functions does not exist`;
-    // if (!(await readdir(join(cwd, '.firebase', site, 'functions'))).length) throw `no files in .firebase/${site}/functions`;
-}
+  execSync('firebase emulators:exec --debug "exit 0"', { cwd });
+  if (
+    await access(join(cwd, '.firebase')).then(
+      () => false,
+      () => true
+    )
+  )
+    throw '.firebase does not exist';
+  if (
+    await access(join(cwd, '.firebase', site)).then(
+      () => false,
+      () => true
+    )
+  )
+    throw `.firebase/${site} does not exist`;
+  if (
+    await access(join(cwd, '.firebase', site, 'hosting')).then(
+      () => false,
+      () => true
+    )
+  )
+    throw `.firebase/${site}/hosting does not exist`;
+  if (!(await readdir(join(cwd, '.firebase', site, 'hosting'))).length)
+    throw `no files in .firebase/${site}/hosting`;
+  // TODO figure out what is wrong
+  // if (await access(join(cwd, '.firebase', site, 'functions')).then(() => false, () => true)) throw `.firebase/${site}/functions does not exist`;
+  // if (!(await readdir(join(cwd, '.firebase', site, 'functions'))).length) throw `no files in .firebase/${site}/functions`;
+};
 
 run().then(
-    () => exit(0),
-    err => {
-        console.error(err.message || err);
-        const logPath = join(cwd, 'firebase-debug.log');
-        if (existsSync(logPath)) console.log(readFileSync(logPath).toString());
-        exit(1);
-    }
+  () => exit(0),
+  (err) => {
+    console.error(err.message || err);
+    const logPath = join(cwd, 'firebase-debug.log');
+    if (existsSync(logPath)) console.log(readFileSync(logPath).toString());
+    exit(1);
+  }
 );
 
 export {};
