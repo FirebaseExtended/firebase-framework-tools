53c2e0ff26e9bfb712504ac17aa002869d50fa36
Julien Martel <julien@monogram.io>
2023-08-30 16:23:21 -0500

    add debug flag to custom test

--- tools/test-custom.ts
+++ tools/test-custom.ts
@@ -8,7 +8,7 @@ const site = 'nextjs-demo-73e34';
 const cwd = join('e2e', 'custom');
 
 const run = async () => {
-    execSync('firebase emulators:exec "exit 0"', { cwd });
+    execSync('firebase emulators:exec --debug "exit 0"', { cwd });
     if (await access(join(cwd, '.firebase')).then(() => false, () => true)) throw '.firebase does not exist';
     if (await access(join(cwd, '.firebase', site)).then(() => false, () => true)) throw `.firebase/${site} does not exist`;
     if (await access(join(cwd, '.firebase', site, 'hosting')).then(() => false, () => true)) throw `.firebase/${site}/hosting does not exist`;
