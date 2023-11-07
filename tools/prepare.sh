320df9bc6f762f0310421fa51275aafa1915318a
James Daniels <jamesdaniels@google.com>
2023-11-07 11:47:37 -0500

    WIP

--- tools/prepare.sh
+++ tools/prepare.sh
@@ -1,6 +1,6 @@
 SHORT_SHA=$(git rev-parse --short $GITHUB_SHA)
-TAG_TEST="^refs/tags/.+$"
-LATEST_TEST="^[^-]*$"
+TAG_TEST="^refs/tags/[^\@]+\@.+$"
+LATEST_TEST="^[^\@]+\@[^-]+$"
 
 if [[ $GITHUB_REF =~ $TAG_TEST ]]; then
     OVERRIDE_VERSION=${GITHUB_REF/refs\/tags\//}
@@ -10,14 +10,17 @@ if [[ $GITHUB_REF =~ $TAG_TEST ]]; then
         NPM_TAG=next
     fi;
 else
-    OVERRIDE_VERSION=$(node -e "console.log(require('./package.json').version)")-canary.$SHORT_SHA
     NPM_TAG=canary
 fi;
 
-npm --no-git-tag-version --allow-same-version -f version $OVERRIDE_VERSION
-
-npm run build &&
-    echo "npm --no-git-tag-version --allow-same-version -f version $OVERRIDE_VERSION &&
-    sed -i 's/exports\.FIREBASE_FRAMEWORKS_VERSION =.*/exports.FIREBASE_FRAMEWORKS_VERSION = \"$OVERRIDE_VERSION\"/' ./dist/constants.js &&
-    npm publish . --tag $NPM_TAG" > ./dist/publish.sh &&
-    chmod +x ./dist/publish.sh
+npm run build:changed &&
+    for workspace in ./packages/* ; do
+        if [[ $NPM_TAG == "canary" ]]; then
+            OVERRIDE_VERSION=$(node -e "console.log(require('$workspace/package.json').version)")-canary.$SHORT_SHA
+        fi;
+        npm --prefix $workspace --no-git-tag-version --allow-same-version -f version $OVERRIDE_VERSION &&
+        echo "npm publish $workspace --tag $NPM_TAG" > $workspace/dist/publish.sh &&
+        chmod +x $workspace/dist/publish.sh &&
+        cd dist &&
+        npm pack ../$workspace
+    done
