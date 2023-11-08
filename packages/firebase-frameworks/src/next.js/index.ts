2c207fd5af99a621816f69890831c41855816a3a
James Daniels <jamesdaniels@google.com>
2023-11-08 01:21:17 -0500

    missing changes

--- packages/firebase-frameworks/src/next.js/index.ts
+++ packages/firebase-frameworks/src/next.js/index.ts
@@ -27,7 +27,7 @@ export const handle = async (req: Request, res: Response) => {
         nextApp = next({
             dev: false,
             dir: process.cwd(),
-            hostname,
+            hostname: '0.0.0.0',
             port
         });
         nextAppsLRU.set(key, nextApp);
