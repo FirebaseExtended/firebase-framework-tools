7056e2f815c3c926b34f217103919ab6b218fd3e
James Daniels <jamesdaniels@google.com>
2023-11-08 14:13:10 -0500

    Ignore **/dist

--- packages/@apphosting/adapter-nextjs/dist/bin/build.js
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env node
-import { spawnSync } from "child_process";
-const cwd = process.cwd();
-spawnSync("next", ["build"], { cwd, stdio: "inherit" });
--- packages/@apphosting/adapter-nextjs/dist/bin/create.js
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env node
-import { spawnSync } from "child_process";
-const [, , projectDirectory] = process.argv;
-const cwd = process.cwd();
-spawnSync("create-next-app", ["--example", "hello-world", "--ts", "--use-npm", projectDirectory], {
-    cwd,
-    stdio: "inherit",
-});
