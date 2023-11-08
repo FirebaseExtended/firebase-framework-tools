8326263b630ffffae3db31ffec3e67c57b2c62a5
James Daniels <jamesdaniels@google.com>
2023-11-07 20:50:36 -0500

    Cleanup and debug statement for rsync

--- publish.js
+++ publish.js
@@ -1,20 +1,20 @@
 #! /usr/bin/env node
 const { execSync } = require("child_process");
 
-const [,packageToPublish,versionToPublish] = /^refs\/tags\/(.+)\@([^\@]+)$/.exec(process.env.GITHUB_REF ?? "") ?? [];
+const [, packageFromRef, versionFromRef] = /^refs\/tags\/(.+)\@([^\@]+)$/.exec(process.env.GITHUB_REF ?? "") ?? [];
 const ref = process.env.GITHUB_SHA ?? "HEAD";
 const shortSHA = execSync(`git rev-parse --short ${ref}`).toString().trim();
 
-const lernaList= JSON.parse(execSync(`lerna list --json ${packageToPublish ? '' : '--since'}`).toString());
-if (packageToPublish && !lernaList.find(it => it.name === packageToPublish)) throw `Lerna didn't find ${packageToPublish} in this workspace`;
+const lernaList= JSON.parse(execSync(`lerna list --json ${packageFromRef ? '' : '--since'}`).toString());
+if (packageFromRef && !lernaList.find(it => it.name === packageFromRef)) throw `Lerna didn't find ${packageFromRef} in this workspace`;
 
 for (const lerna of lernaList) {
     if (lerna.private) continue;
-    if (packageToPublish && packageToPublish !== lerna.name) continue;
-    if (versionToPublish && versionToPublish.split('-')[0] !== lerna.version) throw `Cowardly refusing to publish ${lerna.name}@${versionToPublish} from ${lerna.version}, version needs to be bumped in source.`;
-    const version = versionToPublish || `${lerna.version}-canary.${shortSHA}`;
-    execSync(`npm --prefix ${lerna.location} --no-git-tag-version --allow-same-version -f version ${version}`);
-    const tag = packageToPublish ? (version.includes('-') ? 'next' : 'latest') : 'canary';
-    console.log(`npm publish ${lerna.location} --tag ${tag}`);
-    execSync(`npm --prefix ${lerna.location} --no-git-tag-version --allow-same-version -f version ${lerna.version}`);
+    if (packageFromRef && packageFromRef !== lerna.name) continue;
+    if (versionFromRef && versionFromRef.split('-')[0] !== lerna.version) throw `Cowardly refusing to publish ${lerna.name}@${versionFromRef} from ${lerna.version}, version needs to be bumped in source.`;
+    const version = versionFromRef || `${lerna.version}-canary.${shortSHA}`;
+    const cwd = lerna.location;
+    execSync(`npm --no-git-tag-version --allow-same-version -f version ${version}`, { cwd });
+    const tag = packageFromRef ? (version.includes('-') ? 'next' : 'latest') : 'canary';
+    execSync(`npm publish . --tag ${tag}`, { cwd });
 }
