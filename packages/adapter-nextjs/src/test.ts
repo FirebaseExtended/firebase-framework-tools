f8a12ead6f248308d8b3c3338c1b7a587692bc8b
James Daniels <jamesdaniels@google.com>
2023-11-07 20:31:52 -0500

    More

--- packages/adapter-nextjs/src/test.ts
+++ packages/adapter-nextjs/src/test.ts
@@ -1,18 +1,29 @@
+#! /usr/bin/env node
 import { rmdir } from 'fs/promises';
-import { join } from 'path';
 import { existsSync } from 'fs';
 import { execSync } from 'child_process';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { parse } from 'semver';
+
+const packageJson = JSON.parse((await readFile('package.json')).toString());
+
+// TODO compare against latest & tags
+const version = parse(packageJson.version);
+if (!version) throw "couldn't parse package.json version";
+
+const createNextAppVersionRange = packageJson.dependencies['create-next-app'];
+const nextVersionRange = packageJson.peerDependencies['next'];
+
+const expectedRange = `~${version.major}.${version.minor}.0`;
+if (expectedRange !== createNextAppVersionRange) throw `expected create-next-app version requirement to equal ${expectedRange}`;
+if (expectedRange !== nextVersionRange )  throw `expected next version requirement to equal ${expectedRange}`;
 
 const cwd = process.cwd();
 const projectDir = join(cwd, 'e2e');
 
-const run = async () => {
-    await rmdir(projectDir, { recursive: true }).catch(() => {});
-    execSync(`npx -y -p . apphosting-adapter-nextjs-create ${projectDir}`, { cwd, stdio: 'inherit' });
-    execSync('npx -y -p .. apphosting-adapter-nextjs-build', { cwd: projectDir, stdio: 'inherit' });
-    if (!existsSync(join(projectDir, '.next'))) throw `next app wasn't build`;
-}
+await rmdir(projectDir, { recursive: true }).catch(() => {});
 
-run();
-
-export {};
+execSync(`npx -y -p . apphosting-adapter-nextjs-create ${projectDir}`, { cwd, stdio: 'inherit' });
+execSync('npx -y -p .. apphosting-adapter-nextjs-build', { cwd: projectDir, stdio: 'inherit' });
+if (!existsSync(join(projectDir, '.next'))) throw `next app wasn't build`;
---
+++ publish.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import { execSync } from "child_process"
+
+interface LearnaList {
+    name: string;
+    version: string;
+    private: boolean;
+    location: string;
+};
+
+const [,packageToPublish,versionToPublish] = /^refs\/tags\/(.+)\@([^\@]+)$/.exec(process.env.GITHUB_REF ?? "") ?? [];
+const ref = process.env.GITHUB_SHA ?? "HEAD";
+const shortSHA = execSync(`git rev-parse --short ${ref}`).toString().trim();
+
+const lernaList: LearnaList[] = JSON.parse(execSync(`lerna list --json ${packageToPublish ? '' : '--since'}`).toString());
+if (packageToPublish && !lernaList.find(it => it.name === packageToPublish)) throw `Lerna didn't find ${packageToPublish} in this workspace`;
+
+for (const lerna of lernaList) {
+    if (lerna.private) continue;
+    if (packageToPublish && packageToPublish !== lerna.name) continue;
+    if (versionToPublish && versionToPublish.split('-')[0] !== lerna.version) throw `Cowardly refusing to publish ${lerna.name}@${versionToPublish} from ${lerna.version}, version needs to be bumped in source.`;
+    const version = versionToPublish || `${lerna.version}-canary.${shortSHA}`;
+    execSync(`npm --prefix ${lerna.location} --no-git-tag-version --allow-same-version -f version ${version}`);
+    const tag = packageToPublish ? (version.includes('-') ? 'next' : 'latest') : 'canary';
+    execSync(`npm publish ${lerna.location} --tag ${tag}`);
+    execSync(`npm --prefix ${lerna.location} --no-git-tag-version --allow-same-version -f version ${lerna.version}`);
+}
