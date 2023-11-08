5b34d92859b7ab1c8a3291841862bed835ff61fc
James Daniels <jamesdaniels@google.com>
2023-11-08 03:00:35 -0500

    different release tags

--- publish.js
+++ publish.js
@@ -1,14 +1,14 @@
 #! /usr/bin/env node
 const { execSync } = require("child_process");
 const { writeFileSync, readFileSync } = require("fs");
-const { join } = require("path");
+const { join, basename } = require("path");
 
-const [, packageFromRef, versionFromRef] = /^refs\/tags\/(.+)\@([^\@]+)$/.exec(process.env.GITHUB_REF ?? "") ?? [];
+const [, packageFromRef, versionFromRef] = /^refs\/tags\/(.+)-v(\d*\.\d(\.\d)?(-.+)?)$/.exec(process.env.GITHUB_REF ?? "") ?? [];
 const ref = process.env.GITHUB_SHA ?? "HEAD";
 const shortSHA = execSync(`git rev-parse --short ${ref}`).toString().trim();
 
 const lernaList= JSON.parse(execSync(`lerna list --json ${packageFromRef ? '' : '--since'}`).toString());
-if (packageFromRef && !lernaList.find(it => it.name === packageFromRef)) {
+if (packageFromRef && !lernaList.find(it => basename(it.location) === packageFromRef)) {
     throw `Lerna didn't find ${packageFromRef} in this workspace`;
 }
 
@@ -24,13 +24,13 @@ wombatDressingRoomTokens.forEach((token, pkg) => {
 
 for (const lerna of lernaList) {
     if (lerna.private) continue;
-    if (packageFromRef && packageFromRef !== lerna.name) continue;
+    if (packageFromRef && packageFromRef !== basename(lerna.location)) continue;
     if (versionFromRef && versionFromRef.split('-')[0] !== lerna.version) {
         throw `Cowardly refusing to publish ${lerna.name}@${versionFromRef} from ${lerna.version}, version needs to be bumped in source.`;
     }
     const version = versionFromRef || `${lerna.version}-canary.${shortSHA}`;
     const cwd = lerna.location;
-    const tag = packageFromRef ? (version.includes('-') ? 'next' : 'latest') : 'canary';
+    const tag = versionFromRef ? (version.includes('-') ? 'next' : 'latest') : 'canary';
     const packageJsonPath = join(lerna.location, 'package.json');
     const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
     packageJson.version = version;
