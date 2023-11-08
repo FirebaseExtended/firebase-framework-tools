ddecc8abc4575db511caf2e54dda87f15fdb5dbd
James Daniels <jamesdaniels@google.com>
2023-11-08 01:11:21 -0500

    Cleanup

--- publish.js
+++ publish.js
@@ -8,7 +8,9 @@ const ref = process.env.GITHUB_SHA ?? "HEAD";
 const shortSHA = execSync(`git rev-parse --short ${ref}`).toString().trim();
 
 const lernaList= JSON.parse(execSync(`lerna list --json ${packageFromRef ? '' : '--since'}`).toString());
-if (packageFromRef && !lernaList.find(it => it.name === packageFromRef)) throw `Lerna didn't find ${packageFromRef} in this workspace`;
+if (packageFromRef && !lernaList.find(it => it.name === packageFromRef)) {
+    throw `Lerna didn't find ${packageFromRef} in this workspace`;
+}
 
 const wombatDressingRoomTokens = new Map([
     // Disabling this until I can get wombat access to this org
@@ -23,7 +25,9 @@ wombatDressingRoomTokens.forEach((token, pkg) => {
 for (const lerna of lernaList) {
     if (lerna.private) continue;
     if (packageFromRef && packageFromRef !== lerna.name) continue;
-    if (versionFromRef && versionFromRef.split('-')[0] !== lerna.version) throw `Cowardly refusing to publish ${lerna.name}@${versionFromRef} from ${lerna.version}, version needs to be bumped in source.`;
+    if (versionFromRef && versionFromRef.split('-')[0] !== lerna.version) {
+        throw `Cowardly refusing to publish ${lerna.name}@${versionFromRef} from ${lerna.version}, version needs to be bumped in source.`;
+    }
     const version = versionFromRef || `${lerna.version}-canary.${shortSHA}`;
     const cwd = lerna.location;
     const tag = packageFromRef ? (version.includes('-') ? 'next' : 'latest') : 'canary';
@@ -31,6 +35,8 @@ for (const lerna of lernaList) {
     const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
     packageJson.version = version;
     writeFileSync(packageJsonPath, JSON.stringify(packageJson, undefined, 2));
-    const registry = wombatDressingRoomTokens.get(lerna.name) ? `https://wombat-dressing-room.appspot.com/${lerna.name}/_ns` : 'https://registry.npmjs.org';
+    const registry = wombatDressingRoomTokens.get(lerna.name) ?
+        `https://wombat-dressing-room.appspot.com/${lerna.name}/_ns` :
+        'https://registry.npmjs.org';
     execSync(`npm publish --registry ${registry} --access public --tag ${tag} --provenance`, { cwd });
 }
