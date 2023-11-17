bc30c1feedeeabbf567b044fe68fbed82e513e90
James Daniels <jamesdaniels@google.com>
2023-11-16 21:14:22 -0500

    More stuffs

--- packages/@apphosting/build/src/bin/build.ts
+++ packages/@apphosting/build/src/bin/build.ts
@@ -1,50 +1,56 @@
 #! /usr/bin/env node
-import { spawnSync, spawn } from "child_process";
+import { spawn } from "child_process";
 import { program } from "commander";
 import { parse as semverParse } from "semver";
-import { red, yellow, bold } from "colorette";
+import { yellow, bgRed, bold } from "colorette";
+// @ts-expect-error TODO add interface
+import pickManifest from "npm-pick-manifest";
+import Arborist from "@npmcli/arborist";
 
-const frameworkAliases = new Map([
+// If a framework's NPM module can be named something different, put it in the map
+const frameworkPackageNames = new Map([
     ["nextjs", ["next"]],
+    ["angular", ["@angular/core"]],
 ]);
 
 program
   .option('--framework <string>')
+  .option('--permit-prerelease')
   .argument('<directory>', "path to the project's root directory")
-  .action(async (cwd, { framework }: { framework?: string }) => {
-    if (!framework) {
-        throw new Error("Discovery not implemented. Must provide an option to --framework <string>");
-    }
-    const npmModules = frameworkAliases.get(framework) || [framework];
-    // TODO this search is naive, make it more robust, add types (zod?)
-    const result = JSON.parse(spawnSync("npm", ["ls", ...npmModules, "--json"], { cwd }).stdout.toString());
-    const packageName = npmModules.find(pkg => result.dependencies?.[pkg]);
-    const version = packageName && semverParse(result.dependencies[packageName].version);
-    if (!version) {
-        throw new Error(`Couldn't find ${framework}.`);
-    }
+  .action(async (cwd, options: { framework?: string, permitPrerelease?: boolean }) => {
+    const { framework } = options;
+    if (!framework) throw new Error("Discovery not implemented. Must provide an option to --framework <string>");
+
+    // Find the matching NPM package version
+    const possiblePackageNames = frameworkPackageNames.get(framework) || [framework];
+    const aboristTree = await new Arborist({ path: cwd }).loadActual();
+    const packageName = possiblePackageNames.find(pkg => aboristTree.children.has(pkg));
+    if (!packageName) throw new Error(`Couldn't find ${framework}`);
+    // TODO use NPM semver, then we don't have to worry about null
+    const packgeVersion = semverParse(aboristTree.children.get(packageName)!.pkgid.split(`${packageName}@`)[1]);
+    if (!packgeVersion) throw new Error(`Couldn't find ${framework}.`);
+
+    // Look up a matching @apphosting/adapter-*
+    const permitPrerelease = packgeVersion.prerelease.length > 0 || options.permitPrerelease;
     const adapterName = `@apphosting/adapter-${framework}`;
-    // TODO add types here (zod?), add a search pattern, just using @next for now
-    let npmInfo = JSON.parse(spawnSync("npm", ["info", `${adapterName}@next`, "--json"]).stdout.toString());
-    if (npmInfo.error) {
-        throw new Error(npmInfo.error.detail)
-    }
-    const adapterVersion = semverParse(npmInfo.version);
-    if (!adapterVersion) throw new Error(`Unable to parse ${adapterVersion}`);
-    // TODO actually write a reasonable error message here & use a generator function
-    if (adapterVersion.major !== version.major) {
-        console.error(red(bold(' !')), red('Warning:'), `Using a potentially incompatible adapter ${npmInfo.name}@${npmInfo.version}\n            This is intended to be used with ${packageName} v${adapterVersion.major} but found v${version.major}. You're likely to encounter issues.\n`);
-    } else if (adapterVersion.prerelease) {
-        if (adapterVersion.minor === version.minor) {
-            console.error(red(bold(' !')), red('Warning:'), `Using pre-release platform adapter ${npmInfo.name}@${npmInfo.version}\n            You're likely to encounter issues.\n`);
-        } else {
-            console.error(red(bold(' !')), red('Warning:'), `Using pre-release platform adapter ${npmInfo.name}@${npmInfo.version}\n            This is intended to be used with ${packageName} v${adapterVersion.major}.${adapterVersion.minor} but found v${version.major}.${version.minor}. You're likely to encounter issues.\n`);
-        }
-    } else if (adapterVersion.minor !== version.minor) {
-        console.warn(yellow(bold(' !')), yellow('Warning:'), `Using a potentially incompatible adapter ${npmInfo.name}@${npmInfo.version}\n            This is intended to be used with ${packageName} v${adapterVersion.major}.${adapterVersion.minor} but found v${version.major}.${version.minor}. You may encounter issues.\n`);
-    }
+    const packumentResponse = await fetch(`https://registry.npmjs.org/${adapterName}`);
+    if (!packumentResponse.ok) throw new Error(`Something went wrong fetching ${adapterName}`);
+    const packument = await packumentResponse.json();
+    // TODO find tune this
+    const pickOrder = [
+        `~${packgeVersion.major}.${packgeVersion.minor}.0`,
+        permitPrerelease && `~${packgeVersion.major}.${packgeVersion.minor}.0-next.0`,
+        `^${packgeVersion.major}.0.0`,
+        permitPrerelease && `^${packgeVersion.major}.0.0-next.0`,
+        ">0",
+    ];
+    const { version: adapterVersion } = pickManifest(packument, pickOrder.filter(it => !!it).join(' || '));
+    
+    console.log(' 🔥', bgRed(` ${adapterName}@${yellow(bold(adapterVersion))} `), "\n");
+
+    // Call it via NPX
     const buildCommand = `apphosting-adapter-${framework}-build`;
-    spawn('npx', ['-y', '-p', `${adapterName}@${adapterVersion.raw}`, buildCommand], { cwd, shell: true, stdio: 'inherit' });
+    spawn('npx', ['-y', '-p', `${adapterName}@${adapterVersion}`, buildCommand], { cwd, shell: true, stdio: 'inherit' });
   });
 
 program.parse();
---
+++ packages/@apphosting/discover/src/bin/discover.ts
@@ -0,0 +1,128 @@
+#! /usr/bin/env node
+import { program } from "commander";
+import fsExtra from "fs-extra";
+import YarnLockfile from '@yarnpkg/lockfile';
+import { parse as parseYaml } from "yaml";
+import { performance } from "node:perf_hooks";
+
+const FRAMEWORK_DEFINITIONS = [
+  ["nodejs", [
+    ["nextjs", { requiredPackages: ["next"], requiredFiles: [], bundles: ["react"] }],
+    ["angular", { requiredPackages: ["@angular/core"], requiredFiles: ["angular.json"], bundles: ["vite"] }],
+    // TODO support the other file extensions
+    ["astro", { requiredPackages: ["astro"], requiredFiles: ["astro.config.js", "astro.config.mjs"], bundles: ["lit", "react", "preact", "svelte", "vue", "vite"] }],
+    ["nuxt", { requiredPackages: ["nuxt"], requiredFiles: ["nuxt.config.js"], bundles: ["vue"] }],
+    ["lit", { requiredPackages: ["lit", "lit-element"], requiredFiles: [], bundles: []}],
+    ["vue",  { requiredPackages: ["vue"], requiredFiles: [], bundles: [] }],
+    ["vite", { requiredPackages: ["vite"], requiredFiles: [], bundles: ["vue", "react", "preact", "lit", "svelte"] }],
+    ["preact", { requiredPackages: ["preact"], requiredFiles: [], bundles: [] }],
+    ["react", { requiredPackages: ["react", "react-dom"], requiredFiles: [], bundles: []}],
+    ["svelte", { requiredPackages: ["svelte"], requiredFiles: [], bundles: [] }],
+    ["sveltekit", { requiredPackages: ["@sveltejs/kit"], requiredFiles: [], bundles: ["svelte", "vite"] }]
+  ]]
+] as const;
+
+// TODO support npm-shrinkwrap.json
+const PACKAGE_MANAGER_LOCKFILES = [
+  ['nodejs', [
+    ['npm', ['package-lock.json']],
+    ['yarn', ['yarn.lock']],
+    ['pnpm', ['pnpm-lock.yaml']],
+  ]]
+] as const;
+
+program
+  .option('--github-token <string>')
+  .option('--github-repo <string>')
+  .argument('<directory>', "path to the project's root directory")
+  .action(async (path, { githubRepo, githubToken }: { githubRepo?: string, githubToken?: string }) => {
+    if (githubRepo && !githubToken) throw new Error('needs token');
+
+    const { join } = await (githubRepo ? import('node:path') : import('node:path/posix'));
+
+    const { readFile, pathExists, readJson } = githubRepo ? {
+      readFile: async function(path: string) {
+        const response = await fetch(`https://api.github.com/repos/${githubRepo}/contents/${path}`, {
+          headers: { authorization: `Bearer ${githubToken}`, accept: "application/vnd.github.raw" },
+        });
+        if (!response.ok) throw new Error('fail.');
+        return Buffer.from(await response.text());
+      },
+      pathExists: async function(path: string) {
+        const response = await fetch(`https://api.github.com/repos/${githubRepo}/contents/${path}`, {
+          method: "HEAD",
+          headers: { authorization: `Bearer ${githubToken}`, accept: "application/vnd.github.raw" },
+        });
+        return response.ok;
+      },
+      readJson: async function(path: string) {
+        const response = await fetch(`https://api.github.com/repos/${githubRepo}/contents/${path}`, {
+          headers: { authorization: `Bearer ${githubToken}`, accept: "application/vnd.github.raw" },
+        });
+        if (!response.ok) throw new Error('fail.');
+        return await response.json();
+      },
+    } : fsExtra;
+
+    const discoveredFrameworks: Array<{ framework: string, version: string, packageManager: string, platform: string, bundledWith?: string[] }> = [];
+
+    await Promise.all(PACKAGE_MANAGER_LOCKFILES.map(async ([platform, packageManagerLockfiles]) => {
+      const frameworkDefinitions = FRAMEWORK_DEFINITIONS.find(([it]) => it === platform)?.[1] || [];
+      await Promise.all(packageManagerLockfiles.map(async ([packageManager, possibleLockfiles]) => {
+        const possibleLockfilesExist = await Promise.all(possibleLockfiles.map(it => pathExists(join(path, it))));
+        const [lockfile] = possibleLockfilesExist.map((exists, index) => exists ? possibleLockfiles[index] : undefined).filter(it => !!it);
+        if (!lockfile) return;
+        let packages = new Map<string,string>();
+        if (lockfile === "package-lock.json") {
+          const packageJSON = await readJson(join(path, lockfile));
+          packages = new Map(Object.keys(packageJSON.packages).map(pkg => {
+            const name = pkg.replace(/^node_modules\//, "");
+            const version: string = packageJSON.packages[pkg].version;
+            return [name, version];
+          }));
+        } else if (lockfile === "yarn.lock") {
+          const file = await readFile(join(path, lockfile));
+          const yarnLock = YarnLockfile.parse(file.toString());
+          if (yarnLock.type !== "success") throw new Error(`unable to read ${lockfile}`);
+          packages = new Map(Object.keys(yarnLock.object).map(pkg => {
+            const parts = pkg.split("@");
+            const version = parts.pop()!;
+            return [parts.join("@"), version];
+          }));
+        } else if (lockfile === "pnpm-lock.yaml") {
+          const file = await readFile(join(path, lockfile));
+          const pnpmLock = parseYaml(file.toString());
+          packages = new Map(Object.keys(pnpmLock.packages).map(pkg => {
+            const parts = pkg.replace(/^\//, "").split("(")[0].split("@");
+            const version = parts.pop()!;
+            return [parts.join("@"), version];
+          }));
+        }
+  
+        for (const [framework, { requiredPackages, requiredFiles=[] }] of frameworkDefinitions) {
+          const requiredPackagePresent = requiredPackages.some(it => packages.has(it));
+          if (!requiredPackagePresent) continue;
+          const requiredFileExist = requiredFiles.length === 0 || (await Promise.all(requiredFiles.map(it => pathExists(join(path, it))))).some(it => it);
+          if (!requiredFileExist) continue;
+          const [packageName] = requiredPackages;
+          if (packageName) discoveredFrameworks.push({framework, version: packages.get(packageName)!, packageManager, platform });
+        };
+      }));
+    }));
+
+    for (const { framework, platform } of discoveredFrameworks) {
+      const definition = FRAMEWORK_DEFINITIONS.find(([id]) => id === platform)![1].find(([id]) => id === framework)![1];
+      for (const frameworkToBundle of definition.bundles) {
+        const discovery = discoveredFrameworks.find(({framework}) => framework === frameworkToBundle);
+        if (discovery) {
+          discovery.bundledWith ||= [];
+          discovery.bundledWith.push(framework);
+        }
+      }
+    }
+
+    process.stdout.write(Buffer.from(JSON.stringify({ discovered: discoveredFrameworks }, undefined, 2)));
+    console.log(`\nDone in ${performance.now()}ms`);
+  });
+
+program.parse();
