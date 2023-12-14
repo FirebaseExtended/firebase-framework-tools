a84b7b46768da514bfcf2c76abe65676eece01e7
James Daniels <jamesdaniels@google.com>
2023-12-13 20:53:02 -0700

    Lint on create

--- packages/@apphosting/adapter-angular/src/bin/create.ts
+++ packages/@apphosting/adapter-angular/src/bin/create.ts
@@ -9,37 +9,47 @@ const main = isMain(import.meta);
 
 // TODO can we write a test to see if we're capturing all options from --help?
 const enum Options {
-  help = '--help',
-  interactive = '--interactive',
-  dryRun = '--dry-run',
-  defaults = '--defaults',
-  force = '--force',
-  collection = '--collection',
-  commit = '--commit',
-  createApplication = '--create-application',
-  directory = '--directory',
-  inlineStyle = '--inline-style',
-  inlineTemplate = '--inline-template',
-  minimal = '--minimal',
-  newProjecRoot = '--new-project-root',
-  packageManager = '--package-manager',
-  prefix = '--prefix',
-  routing = '--routing',
-  skipGit = '--skip-git',
-  skipInstall = '--skip-install',
-  skipTests = '--skip-tests',
-  ssr = '--ssr',
-  standalone = '--standalone',
-  strict = '--strict',
-  style = '--style',
-  viewEncapsulation = '--view-encapsulation',
-};
+  help = "--help",
+  interactive = "--interactive",
+  dryRun = "--dry-run",
+  defaults = "--defaults",
+  force = "--force",
+  collection = "--collection",
+  commit = "--commit",
+  createApplication = "--create-application",
+  directory = "--directory",
+  inlineStyle = "--inline-style",
+  inlineTemplate = "--inline-template",
+  minimal = "--minimal",
+  newProjecRoot = "--new-project-root",
+  packageManager = "--package-manager",
+  prefix = "--prefix",
+  routing = "--routing",
+  skipGit = "--skip-git",
+  skipInstall = "--skip-install",
+  skipTests = "--skip-tests",
+  ssr = "--ssr",
+  standalone = "--standalone",
+  strict = "--strict",
+  style = "--style",
+  viewEncapsulation = "--view-encapsulation",
+}
 
-export async function create(projectDirectory=process.argv[2], cwd=process.cwd()) {
+export async function create(projectDirectory = process.argv[2], cwd = process.cwd()) {
   return await new Promise<void>((resolve, reject) => {
-    const args = ['-y', '-p', `@angular/cli@${ANGULAR_CLI_VERSION}`, 'ng', 'new', 'hello-world', Options.directory, projectDirectory, Options.skipGit];
-    const process = spawn('npx', args, { cwd, shell: true, stdio: "inherit" });
-    process.on("exit", code => {
+    const args = [
+      "-y",
+      "-p",
+      `@angular/cli@${ANGULAR_CLI_VERSION}`,
+      "ng",
+      "new",
+      "hello-world",
+      Options.directory,
+      projectDirectory,
+      Options.skipGit,
+    ];
+    const process = spawn("npx", args, { cwd, shell: true, stdio: "inherit" });
+    process.on("exit", (code) => {
       if (code === 0) return resolve();
       reject();
     });
--- packages/@apphosting/adapter-nextjs/src/bin/create.ts
+++ packages/@apphosting/adapter-nextjs/src/bin/create.ts
@@ -9,22 +9,22 @@ const main = isMain(import.meta);
 
 // TODO can we write a test to see if we're capturing all options from --help?
 const enum Options {
-  typescript = '--typescript',
-  javascript = '--javascript',
-  tailwind = '--tailwind',
-  eslint = '--eslint',
-  app = '--app',
-  srcDir = '--src-dir',
-  importAlias = '--import-alias',
-  example = '--example',
-  examplePath = '--example-path',
-  useNpm = '--use-npm',
-  useYarn = '--use-yarn',
-  useBun = '--use-bin',
-  usePnpm = '--use-pnpm',
-};
+  typescript = "--typescript",
+  javascript = "--javascript",
+  tailwind = "--tailwind",
+  eslint = "--eslint",
+  app = "--app",
+  srcDir = "--src-dir",
+  importAlias = "--import-alias",
+  example = "--example",
+  examplePath = "--example-path",
+  useNpm = "--use-npm",
+  useYarn = "--use-yarn",
+  useBun = "--use-bin",
+  usePnpm = "--use-pnpm",
+}
 
-export async function create(projectDirectory=process.argv[2], cwd=process.cwd()) {
+export async function create(projectDirectory = process.argv[2], cwd = process.cwd()) {
   return await new Promise<void>((resolve, reject) => {
     const typescript = true;
     const packageManager = "npm";
@@ -33,10 +33,18 @@ export async function create(projectDirectory=process.argv[2], cwd=process.cwd()
     const app = false;
     const srcDir = false;
     const importAlias = undefined;
-    const example = 'hello-world';
+    const example = "hello-world";
     const examplePath = undefined;
     // TODO create-next-app doesn't like an existing directory, that includes a firebase-debug.log use tmpdir & move
-    const args = ['-y', '-p', `create-next-app@${CREATE_NEXT_APP_VERSION}`, 'create-next-app', projectDirectory, `--use-${packageManager}`, typescript ? Options.typescript : Options.javascript];
+    const args = [
+      "-y",
+      "-p",
+      `create-next-app@${CREATE_NEXT_APP_VERSION}`,
+      "create-next-app",
+      projectDirectory,
+      `--use-${packageManager}`,
+      typescript ? Options.typescript : Options.javascript,
+    ];
     if (tailwind) args.push(Options.tailwind);
     if (eslint) args.push(Options.eslint);
     if (app) args.push(Options.app);
@@ -44,8 +52,8 @@ export async function create(projectDirectory=process.argv[2], cwd=process.cwd()
     if (importAlias) args.push(Options.importAlias, importAlias);
     if (example) args.push(Options.example, example);
     if (examplePath) args.push(Options.examplePath, examplePath);
-    const process = spawn('npx', args, { cwd, shell: true, stdio: "inherit" });
-    process.on("exit", code => {
+    const process = spawn("npx", args, { cwd, shell: true, stdio: "inherit" });
+    process.on("exit", (code) => {
       if (code === 0) return resolve();
       reject();
     });
