1074f7a6bc8bdb055c1ceee31cf36e88f3942d44
Leonardo Ortiz <leo@monogram.io>
2024-05-06 14:27:12 -0300

    keep only firebase logo in Next.js starter (#175)

--- starters/nextjs/basic/src/app/components/index.tsx
+++ starters/nextjs/basic/src/app/components/index.tsx
@@ -2,4 +2,3 @@ export { Arrow } from "./Arrow";
 export { ArrowBox } from "./ArrowBox";
 export { Firebase } from "./Firebase";
 export { Header } from "./Header";
-export { NextJs } from "./NextJs";
