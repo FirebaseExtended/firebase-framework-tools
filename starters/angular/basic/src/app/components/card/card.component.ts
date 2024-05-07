a49debd85ac85f41bfee548a5a6bd2e9c87aa747
Paweł Kubiak <43759569+9kubczas4@users.noreply.github.com>
2024-05-07 19:45:54 +0200

    feat: Angular boilerplate (#174)
    * feat: Angular boilerplate
    
    * feat: remove links container
    
    * feat: revert removing styles for links
    
    * feat: update titles
    
    * Update starters/angular/basic/README.md
    
    Co-authored-by: Leonardo Ortiz <leo@monogram.io>
    
    * Update starters/angular/basic/server.ts
    
    Co-authored-by: Leonardo Ortiz <leo@monogram.io>
    
    ---------
    
    Co-authored-by: Leonardo Ortiz <leo@monogram.io>
    Co-authored-by: Yuangwang <yuangwang2@gmail.com>

---
+++ starters/angular/basic/src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { UpdateContentIfNotGeneratedByServerDirective } from '../../directives/update-content-if-not-generated-by-server.directive';
+import { getDateString, getRandomUUID } from '../../utils/metadata-generator';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [UpdateContentIfNotGeneratedByServerDirective],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  dateString = signal(getDateString());
+  randomUUID = signal(getRandomUUID());
+}
