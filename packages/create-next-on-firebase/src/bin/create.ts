ec27570d10f3c88559dcb6dd73125665e76be872
James Daniels <jamesdaniels@google.com>
2024-05-13 15:10:24 -0400

    deps

--- packages/create-next-on-firebase/src/bin/create.ts
+++ packages/create-next-on-firebase/src/bin/create.ts
@@ -1,33 +1,14 @@
 #! /usr/bin/env node
-import { spawn } from "node:child_process";
 import { program } from "commander";
+import spawn from "@npmcli/promise-spawn";
 
-program.argument("<directory>", "path to the project's root directory").action((directory) => {
-  console.log(`Shelling out to @apphosting/create:`);
-
-  const createProcess = spawn("npx", ["@apphosting/create", "--framework=nextjs", directory], {
-    shell: true,
-    stdio: "inherit",
+program
+  .argument("<directory>", "path to the project's root directory")
+  .action(async (directory) => {
+    await spawn("npx", ["@apphosting/create", "--framework=nextjs", directory], {
+      shell: true,
+      stdio: "inherit",
+    });
   });
-
-  // print out the shell command that spawn created
-  console.log(createProcess.spawnargs.at(-1));
-
-  createProcess.stdout?.on("data", (data) => {
-    console.log(`i: ${data}`);
-  });
-
-  createProcess.stderr?.on("data", (data) => {
-    console.error(`error: ${data}`);
-  });
-
-  createProcess.on("close", (code) => {
-    if (code === 0) {
-      console.log("Success!");
-    } else {
-      console.log(`There was a problem, exited with code ${code}`);
-    }
-  });
-});
 
 program.parse();
