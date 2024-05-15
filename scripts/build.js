4f4f761c82e6ec43e9864b5fdb5bc3cd41709c90
Wei Wang <weiwng@google.com>
2024-05-15 17:50:47 -0400

    fix scope args

--- scripts/build.js
+++ scripts/build.js
@@ -2,23 +2,7 @@
 const { spawn } = require("child_process");
 const { lernaScopeArgs } = require("./github.js");
 
-const scopeArgs = [
-  "--scope",
-  "@apphosting/adapter-angular",
-  "--scope",
-  "@apphosting/adapter-nextjs",
-  "--scope",
-  "@apphosting/build",
-  "--scope",
-  "@apphosting/create",
-  "--scope",
-  "@apphosting/discover",
-  "--scope",
-  "create-next-on-firebase",
-  "--scope",
-  "firebase-frameworks",
-];
-const buildProcess = spawn("lerna", ["run", "build", ...scopeArgs], { stdio: "inherit" });
+const buildProcess = spawn("lerna", ["run", "build", ...lernaScopeArgs], { stdio: "inherit" });
 
 buildProcess.on("close", (code) => {
   process.exit(code);
