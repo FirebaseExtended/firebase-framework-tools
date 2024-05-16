96f2c966430d8c29efb89848aa980f2ba8116b3e
James Daniels <jamesdaniels@google.com>
2024-05-16 11:51:12 -0400

    Add lerna scopes back in

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,8 @@
 #! /usr/bin/env node
 const { spawn } = require("child_process");
+const { lernaScopeArgs } = require("./github.js");
 
-const buildProcess = spawn("lerna", ["run", "build"], { stdio: "inherit" });
+const buildProcess = spawn("lerna", ["run", "build", ...lernaScopeArgs], { stdio: "inherit" });
 
 buildProcess.on("close", (code) => {
   process.exit(code);
