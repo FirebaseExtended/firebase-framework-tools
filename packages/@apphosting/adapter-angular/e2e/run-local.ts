77b7d6cc9b8e50c51d9ab839050178399458f4c8
James Daniels <jamesdaniels@google.com>
2024-05-16 16:23:07 -0400

    Force an exit 0/1

--- packages/@apphosting/adapter-angular/e2e/run-local.ts
+++ packages/@apphosting/adapter-angular/e2e/run-local.ts
@@ -22,68 +22,75 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const buildScript = join(__dirname, "../dist/bin/build.js");
 const angularJSON = JSON.parse((await readFile(join(cwd, "angular.json"))).toString());
 
-for (const enableSSR of [false, true]) {
-  angularJSON.projects["firebase-app-hosting-angular"].architect.build.ssr = enableSSR && {
-    entry: "server.ts",
-  };
-  await writeFile(join(cwd, "angular.json"), JSON.stringify(angularJSON, null, 2));
+try {
+  for (const enableSSR of [false, true]) {
+    angularJSON.projects["firebase-app-hosting-angular"].architect.build.ssr = enableSSR && {
+      entry: "server.ts",
+    };
+    await writeFile(join(cwd, "angular.json"), JSON.stringify(angularJSON, null, 2));
 
-  await promiseSpawn("node", [buildScript], { cwd, stdio: "inherit", shell: true });
+    await promiseSpawn("node", [buildScript], { cwd, stdio: "inherit", shell: true });
 
-  const bundleYaml = parseYaml((await readFile(join(cwd, ".apphosting/bundle.yaml"))).toString());
+    const bundleYaml = parseYaml((await readFile(join(cwd, ".apphosting/bundle.yaml"))).toString());
 
-  const runCommand = bundleYaml.runCommand;
+    const runCommand = bundleYaml.runCommand;
 
-  if (typeof runCommand !== "string") {
-    throw new Error("runCommand must be a string");
-  }
+    if (typeof runCommand !== "string") {
+      throw new Error("runCommand must be a string");
+    }
 
-  console.log(`> ${runCommand}`);
+    console.log(`> ${runCommand}`);
 
-  const [runScript, ...runArgs] = runCommand.split(" ");
-  let resolveHostname: (it: string) => void;
-  let rejectHostname: () => void;
-  const hostnamePromise = new Promise<string>((resolve, reject) => {
-    resolveHostname = resolve;
-    rejectHostname = reject;
-  });
-  const port = 8080 + Math.floor(Math.random() * 1000);
-  const run = spawn(runScript, runArgs, {
-    cwd,
-    shell: true,
-    env: {
-      NODE_ENV: "production",
-      LOCAL: "1",
-      PORT: port.toString(),
-    },
-  });
-  run.stderr.on("data", (data) => console.error(data.toString()));
-  run.stdout.on("data", (data) => {
-    console.log(data.toString());
-    if (data.toString() === `Node Express server listening on http://localhost:${port}\n`) {
-      resolveHostname(`http://localhost:${port}`);
-    } else {
+    const [runScript, ...runArgs] = runCommand.split(" ");
+    let resolveHostname: (it: string) => void;
+    let rejectHostname: () => void;
+    const hostnamePromise = new Promise<string>((resolve, reject) => {
+      resolveHostname = resolve;
+      rejectHostname = reject;
+    });
+    const port = 8080 + Math.floor(Math.random() * 1000);
+    const run = spawn(runScript, runArgs, {
+      cwd,
+      shell: true,
+      env: {
+        NODE_ENV: "production",
+        LOCAL: "1",
+        PORT: port.toString(),
+      },
+    });
+    run.stderr.on("data", (data) => console.error(data.toString()));
+    run.stdout.on("data", (data) => {
+      console.log(data.toString());
+      if (data.toString() === `Node Express server listening on http://localhost:${port}\n`) {
+        resolveHostname(`http://localhost:${port}`);
+      } else {
+        run.stdin.end();
+        run.kill("SIGKILL");
+      }
+    });
+    run.on("close", (code) => {
+      if (code) {
+        rejectHostname();
+      }
+    });
+    const HOST = await hostnamePromise;
+    console.log("> ts-mocha -p tsconfig.json e2e/**/*.spec.ts");
+    await promiseSpawn("ts-mocha", ["-p", "tsconfig.json", "e2e/**/*.spec.ts"], {
+      shell: true,
+      stdio: "inherit",
+      env: {
+        ...process.env,
+        SSR: enableSSR ? "1" : undefined,
+        HOST,
+      },
+    }).finally(() => {
       run.stdin.end();
       run.kill("SIGKILL");
-    }
-  });
-  run.on("close", (code) => {
-    if (code) {
-      rejectHostname();
-    }
-  });
-  const HOST = await hostnamePromise;
-  console.log("> ts-mocha -p tsconfig.json e2e/**/*.spec.ts");
-  await promiseSpawn("ts-mocha", ["-p", "tsconfig.json", "e2e/**/*.spec.ts"], {
-    shell: true,
-    stdio: "inherit",
-    env: {
-      ...process.env,
-      SSR: enableSSR ? "1" : undefined,
-      HOST,
-    },
-  }).finally(() => {
-    run.stdin.end();
-    run.kill("SIGKILL");
-  });
+    });
+  }
+} catch (e) {
+  console.error(e);
+  process.exit(1);
 }
+
+process.exit(0);
