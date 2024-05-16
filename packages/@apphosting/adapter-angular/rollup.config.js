43025746b81147fae7704b42960d442d9ab91f7d
James Daniels <jamesdaniels@google.com>
2024-05-16 11:32:51 -0400

    Merged simple-server package into adapter-angular

--- packages/@apphosting/adapter-angular/rollup.config.js
+++ packages/@apphosting/adapter-angular/rollup.config.js
@@ -1,22 +1,16 @@
 import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import commonjs from "rollup-plugin-commonjs";
-import path from "path";
 
-import { execSync } from "child_process";
-
-const NPM_ROOT = execSync("npm root").toString().trim();
 export default {
-  input: "server.mjs",
+  input: "./src/simple-server/server.mjs",
   output: {
-    file: "bundled_server.mjs",
+    file: "./dist/simple-server/bundled_server.mjs",
     format: "es",
   },
   plugins: [
     resolve({ preferBuiltins: true }),
     json(),
-    commonjs({
-      include: path.join(NPM_ROOT, "**"),
-    }),
+    commonjs(),
   ],
 };
