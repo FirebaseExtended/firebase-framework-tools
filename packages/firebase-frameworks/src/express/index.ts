14268a1da561544f3cbd7d8d6d8291516bec7582
Leonardo Ortiz <leo@monogram.io>
2024-08-05 15:29:38 -0300

    Revert "Use incomingMessageFromExpress function in express request handler (#234)"
    This reverts commit 6914a0ea4cf91f2677d5e7e1657555bcaf391040.

--- packages/firebase-frameworks/src/express/index.ts
+++ packages/firebase-frameworks/src/express/index.ts
@@ -1,13 +1,10 @@
 import { pathToFileURL } from "url";
 import type { Request } from "firebase-functions/v2/https";
 import type { Response } from "express";
-import { incomingMessageFromExpress } from "../utils.js";
 
 const express = import(`${pathToFileURL(process.cwd())}/bootstrap.js`);
 
 export const handle = async (req: Request, res: Response) => {
   const { handle } = await express;
-  const incomingMessage = incomingMessageFromExpress(req);
-
-  handle(incomingMessage, res);
+  handle(req, res);
 };
