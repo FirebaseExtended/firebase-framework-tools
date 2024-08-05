c0d40282f54aa8ced12b076251d995cbbc810119
Leonardo Ortiz <leo@monogram.io>
2024-08-05 15:56:51 -0300

    reuse dynamic express import

--- packages/firebase-frameworks/src/astro/index.ts
+++ packages/firebase-frameworks/src/astro/index.ts
@@ -1,9 +1,7 @@
-import { pathToFileURL } from "url";
 import type { Request } from "firebase-functions/v2/https";
 import type { Response } from "express";
 import { incomingMessageFromExpress } from "../utils.js";
-
-const express = import(`${pathToFileURL(process.cwd())}/bootstrap.js`);
+import { express } from "../express/index.js";
 
 export const handle = async (req: Request, res: Response) => {
   const { handle } = await express;
