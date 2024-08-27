04d5274e0d44b3dd8db90449e34786fd0916f0fc
sjjj986 <sijinli@google.com>
2024-08-27 18:26:03 -0400

    move assert

--- packages/@apphosting/adapter-angular/src/bin/build.ts
+++ packages/@apphosting/adapter-angular/src/bin/build.ts
@@ -12,15 +12,15 @@ const opts = getBuildOptions();
 
 // Check build conditions, which vary depending on your project structure (standalone or monorepo)
 await checkBuildConditions(opts);
+if (!process.env.FRAMEWORK_VERSION) {
+  throw new Error("Could not find the angular version of the application");
+}
 
 // enable JSON build logs for application builder
 process.env.NG_BUILD_LOGS_JSON = "1";
 const { stdout: output } = await runBuild();
 if (!output) {
   throw new Error("No output from Angular build command, expecting a build manifest file.");
-}
-if (!process.env.FRAMEWORK_VERSION) {
-  throw new Error("Could not find the angular version of the application");
 }
 const outputBundleOptions = parseOutputBundleOptions(output);
 await generateOutputDirectory(root, outputBundleOptions, process.env.FRAMEWORK_VERSION);
