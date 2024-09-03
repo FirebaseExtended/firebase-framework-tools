1a99446b0f178e79f257c27f9e7dbe32fa568230
Yuangwang <yuangwang2@gmail.com>
2024-09-03 16:48:44 -0400

    Add output bundle spec to common module (#244)
    * add output bundle spec to common module
    
    * Update package.json
    
    * packagelock
    
    * address comments
    
    * fix prettier
    
    * add comment
    
    * address comments
    
    * address comments
    
    * prettier
    
    * address comment

--- packages/@apphosting/common/src/index.ts
+++ packages/@apphosting/common/src/index.ts
@@ -1,5 +1,59 @@
 import { spawn } from "child_process";
 
+// Output bundle metadata specifications to be written to bundle.yaml
+export interface OutputBundleConfig {
+  version: "v1";
+  serverConfig: ServerConfig;
+  metadata: Metadata;
+}
+
+// Fields needed to configure the App Hosting server
+interface ServerConfig {
+  // Command to start the server (e.g. "node dist/index.js"). Assume this command is run from the root dir of the workspace
+  runCommand: string;
+  // Environment variables set when the app is run
+  environmentVariables?: EnvVarConfig[];
+  // See https://firebase.google.com/docs/reference/apphosting/rest/v1beta/projects.locations.backends.builds#runconfig for documentation on the next fields
+  // The maximum number of concurrent requests that each server instance can receive.
+  concurrency?: number;
+  // The number of CPUs used in a single server instance.
+  cpu?: number;
+  // The amount of memory available for a server instance.
+  memoryMiB?: number;
+  // The limit on the minimum number of function instances that may coexist at a given time.
+  minInstances?: number;
+  // The limit on the maximum number of function instances that may coexist at a given time.
+  maxInstances?: number;
+}
+
+// Additonal fields needed for identifying the framework and adapter being used
+interface Metadata {
+  // Name of the adapter (this should be the official package name) e.g. "@apphosting/adapter-nextjs"
+  adapterPackageName: string;
+  // Version of the adapter, e.g. "18.0.1"
+  adapterVersion: string;
+  // Name of the framework that is being supported, e.g. "angular"
+  framework: string;
+  // Version of the framework that is being supported, e.g. "18.0.1"
+  frameworkVersion?: string;
+}
+
+// Represents a single environment variable.
+interface EnvVarConfig {
+  // Name of the variable
+  variable: string;
+  // Value associated with the variable
+  value: string;
+  // Where the variable will be available, for now only RUNTIME is supported
+  availability: Availability.Runtime[];
+}
+
+// Represents where environment variables are made available
+enum Availability {
+  // Runtime environment variables are available on the server when the app is run
+  Runtime,
+}
+
 // Options to configure the build of a framework application
 export interface BuildOptions {
   // command to run build script (e.g. "npm", "nx", etc.)
