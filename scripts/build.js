b42ca5c9b59437609a529cc0caabed57aef7be5d
James Daniels <jamesdaniels@google.com>
2024-09-12 11:47:04 -0400

    Build needs --include-dependencies

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,9 @@
 const { spawn } = require("child_process");
 const { lernaScopes } = require("./github.js");
 
-const buildProcess = spawn("lerna", ["run", "build", ...lernaScopes], { stdio: "inherit" });
+const buildProcess = spawn("lerna", ["run", "build", "--include-dependencies", ...lernaScopes], {
+  stdio: "inherit",
+});
 
 buildProcess.on("close", (code) => {
   process.exit(code);
