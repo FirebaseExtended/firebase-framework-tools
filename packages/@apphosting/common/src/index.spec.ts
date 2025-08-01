9c03ce9b0e80e013c026948afdb9285bc59d9888
Joanna Wang <annajowang@gmail.com>
2025-08-01 22:25:53 +0000

    lint

--- packages/@apphosting/common/src/index.spec.ts
+++ packages/@apphosting/common/src/index.spec.ts
@@ -14,7 +14,7 @@ describe("update or create .gitignore", () => {
     fs.rmSync(tmpDir, { recursive: true, force: true });
   });
 
-  it(".gitignore file exists and is correctly updated with missing paths", async () => {
+  it(".gitignore file exists and is correctly updated with missing paths", () => {
     fs.writeFileSync(path.join(tmpDir, ".gitignore"), "existingpath/");
 
     updateOrCreateGitignore(tmpDir, ["existingpath/", "newpath/"]);
@@ -22,7 +22,7 @@ describe("update or create .gitignore", () => {
     const gitignoreContent = fs.readFileSync(path.join(tmpDir, ".gitignore"), "utf-8");
     assert.equal(`existingpath/\nnewpath/`, gitignoreContent);
   });
-  it(".gitignore file does not exist and is created", async () => {
+  it(".gitignore file does not exist and is created", () => {
     updateOrCreateGitignore(tmpDir, ["chickenpath/", "newpath/"]);
     const gitignoreContent = fs.readFileSync(path.join(tmpDir, ".gitignore"), "utf-8");
     assert.equal(`chickenpath/\nnewpath/`, gitignoreContent);
