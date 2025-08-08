4585bf7fe3a5a2289de7a1d47676b8116d62c029
sjjj986 <sijinli@google.com>
2025-08-08 16:36:29 -0400

    lint

--- packages/@apphosting/common/src/index.spec.ts
+++ packages/@apphosting/common/src/index.spec.ts
@@ -4,13 +4,13 @@ import path from "path";
 import os from "os";
 import { getBuildOptions, updateOrCreateGitignore } from "./index";
 
-const originalCwd = process.cwd;
+const originalCwd = process.cwd.bind(process);
 
 describe("get a set of default build options", () => {
-  const mockCwd = '/fake/project/';
+  const mockCwd = "/fake/project/";
   beforeEach(() => {
     process.cwd = () => mockCwd;
-  })
+  });
 
   afterEach(() => {
     process.cwd = originalCwd;
@@ -20,32 +20,39 @@ describe("get a set of default build options", () => {
     delete process.env.MONOREPO_PROJECT;
   });
 
-  it('returns monorepo build options when MONOREPO_COMMAND is set', () => {
-    process.env.MONOREPO_COMMAND = 'turbo';
-    process.env.MONOREPO_BUILD_ARGS = '--filter=web,--env-mode=strict';
-    process.env.GOOGLE_BUILDABLE = '/workspace/apps/web';
-    process.env.MONOREPO_PROJECT = 'web';
+  it("returns monorepo build options when MONOREPO_COMMAND is set", () => {
+    process.env.MONOREPO_COMMAND = "turbo";
+    process.env.MONOREPO_BUILD_ARGS = "--filter=web,--env-mode=strict";
+    process.env.GOOGLE_BUILDABLE = "/workspace/apps/web";
+    process.env.MONOREPO_PROJECT = "web";
 
     // Assert that the returned options are correct
     const expectedOptions = {
-      buildCommand: 'turbo',
-      buildArgs: ['run', 'build', '--filter=web', '--env-mode=strict'],
-      projectDirectory: '/workspace/apps/web',
-      projectName: 'web',
+      buildCommand: "turbo",
+      buildArgs: ["run", "build", "--filter=web", "--env-mode=strict"],
+      projectDirectory: "/workspace/apps/web",
+      projectName: "web",
     };
-    assert.deepStrictEqual(getBuildOptions(), expectedOptions, 'Monorepo build options are incorrect');
+    assert.deepStrictEqual(
+      getBuildOptions(),
+      expectedOptions,
+      "Monorepo build options are incorrect",
+    );
   });
 
-  it('returns standard build options when MONOREPO_COMMAND is not set', () => {
+  it("returns standard build options when MONOREPO_COMMAND is not set", () => {
     const expectedOptions = {
-      buildCommand: 'npm',
-      buildArgs: ['run', 'build'],
+      buildCommand: "npm",
+      buildArgs: ["run", "build"],
       projectDirectory: process.cwd(),
     };
-    assert.deepStrictEqual(getBuildOptions(), expectedOptions, 'Standard build options are incorrect');
+    assert.deepStrictEqual(
+      getBuildOptions(),
+      expectedOptions,
+      "Standard build options are incorrect",
+    );
   });
 });
-
 
 describe("update or create .gitignore", () => {
   let tmpDir: string;
