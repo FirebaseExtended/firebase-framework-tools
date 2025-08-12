1c9bfb496ed4fc57f7960de5b804462ce5e3872a
Joanna Wang <annajowang@gmail.com>
2025-08-12 22:37:11 +0000

    Create apphosting-local-build command.

---
+++ packages/@apphosting/build/src/bin/localbuild.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import { spawn } from "child_process";
+import { program } from "commander";
+import { parse as semverParse } from "semver";
+import { yellow, bgRed, bold } from "colorette";
+// @ts-expect-error TODO add interface
+import pickManifest from "npm-pick-manifest";
+import { discover } from "@apphosting/discover";
+
+program
+  // TODO: add framework option later. For now we support nextjs only.
+  .argument("<directory>", "path to the project's root directory")
+  .action(async () => {
+
+    const projectRoot = program.args[0];
+    const framework = "nextjs";
+    // TODO: We are using the latest framework adapter versions, but in the future
+    // we should parse the framework version and use the matching adapter version.
+    const adapterName = `@apphosting/adapter-nextjs`;
+    const packumentResponse = await fetch(`https://registry.npmjs.org/${adapterName}`);
+    if (!packumentResponse.ok) throw new Error(`Something went wrong fetching ${adapterName}`);
+    const packument = await packumentResponse.json();
+    const adapterVersion = packument["dist-tags"]["canary"];
+    // TODO: should check for existence of adapter in app's package.json and use that version instead.
+
+    console.log(" 🔥", bgRed(` ${adapterName}@${yellow(bold(adapterVersion))} `), "\n");
+
+    // Call it via NPX
+    const buildCommand = `apphosting-adapter-${framework}-build`;
+    spawn("npx", ["-y", "-p", `${adapterName}@${adapterVersion}`, buildCommand], {
+      cwd: projectRoot,
+      shell: true,
+      stdio: "inherit",
+    });
+  });
+
+program.parse();
