8eb011352f46f62919fb2ca30cb0ff0ab0124108
annajowang <31288696+annajowang@users.noreply.github.com>
2025-09-23 14:56:42 -0400

    Move all local build logic into a callable function. (#403)
    Move all local build logic into a callable function

--- packages/@apphosting/build/src/bin/localbuild.ts
+++ packages/@apphosting/build/src/bin/localbuild.ts
@@ -1,23 +1,12 @@
 #! /usr/bin/env node
-import { SupportedFrameworks } from "@apphosting/common";
-import { adapterBuild } from "../index.js";
+import { localBuild } from "../index.js";
 import { program } from "commander";
 
 program
   .argument("<projectRoot>", "path to the project's root directory")
   .option("--framework <framework>")
   .action(async (projectRoot, opts) => {
-    // TODO(#382): support other apphosting.*.yaml files.
-
-    // TODO(#382): parse apphosting.yaml for environment variables / secrets needed during build time.
-    if (opts.framework && SupportedFrameworks.includes(opts.framework)) {
-      // TODO(#382): Skip this if there's a custom build command in apphosting.yaml.
-      await adapterBuild(projectRoot, opts.framework);
-    }
-
-    // TODO(#382): Parse apphosting.yaml to set custom run command in bundle.yaml
-    // TODO(#382): parse apphosting.yaml for environment variables / secrets needed during runtime to include in the bunde.yaml
-    // TODO(#382): parse apphosting.yaml for runConfig to include in bundle.yaml
+    await localBuild(projectRoot, opts.framework);
   });
 
 program.parse();
